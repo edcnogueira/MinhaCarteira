e52a48d325fb2725a24e584995613fb737cc5690
edcnogueira <edjan.nogueira14@gmail.com>
2022-01-18 13:45:10 -0300

    adiciona parte do header e trabalha com alguns emojis do html

--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -4,4 +4,19 @@ export const Container = styled.div`
   grid-area: MH;
 
   background-color: ${props => props.theme.color.secondary};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+
+  border-bottom: 1px solid ${props => props.theme.color.gray};
 `;
+
+export const Profile = styled.div`
+  color: ${props => props.theme.color.white};
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
